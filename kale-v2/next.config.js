d7fc31ab151fe7e58cf31074e05a5ef170857c8a
kale <18kmkf@queensu.ca>
2023-09-23 15:48:56 -0400

    Update config

--- kale-v2/next.config.js
+++ kale-v2/next.config.js
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {
-    target: 'experimental-serverless-trace',
-    exportPathMap: function () {
-      return {
-        '/': { page: '/' }, // Replace with your page routes
-        '/about': { page: '/about' },
-        '/canadarevenueagency': { page: '/canadarevenueagency' },
-        '/cisc226': { page: '/cisc226' },
-        '/compsa': { page: '/compsa' },
-        '/orientation': { page: '/orientation' },
-        '/statisticscanada': { page: '/statisticscanada' },
-        '/studentpeermentor': { page: '/studentpeermentor' },
-        '/studentsubcommittee': { page: '/studentsubcommittee' },
-      };
-    },
-  };
+module.exports = nextConfig
