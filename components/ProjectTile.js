883d57df85a85dc7af2837d4a0681e4ae6f4e994
kale <18kmkf@queensu.ca>
2024-09-03 15:41:46 -0400

    update project tile

--- components/ProjectTile.js
+++ components/ProjectTile.js
@@ -1,20 +1,35 @@
 import React from "react";
 import Image from "next/image";
-const ProjectTile = ({ subtitle, title, description, thumbnail }) => {
+const ProjectTile = ({
+  subtitle,
+  title,
+  description,
+  contributions = [],
+  thumbnail,
+}) => {
   return (
     <div className="bg-[#1C2327] border border-1 border-gray-400 rounded-xl">
       <div className="flex flex-row">
         <div className="w-1/2 p-4">
-          <p className="text-[18px] text-secondary-col">{subtitle}</p>
-          <h1 className="text-[25px] font-semibold">{title}</h1>
-          <p className="text-[16px]">{description}</p>
+          {/* <p className="text-[18px] text-secondary-col">{subtitle}</p> */}
+          <h1 className="text-[25px] font-semibold mt-2">{title}</h1>
+          <h3 className="mt-2">Project Overview</h3>
+          <p className="text-[16px] text-secondary-col mt-2">{description}</p>
+          <div>
+            <h3 className=" mt-4">Key Contributions</h3>
+            <ul className="list-disc ml-4 mt-2 text-secondary-col">
+              {contributions.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div className="relative overflow-hidden">
           <Image
             src={thumbnail}
             width={500}
             height={175}
-            className="transition-transform duration-300 ease-in-out transform hover:scale-110 origin-left"
+            // className="transition-transform duration-300 ease-in-out transform hover:scale-110 origin-left"
           />
         </div>
       </div>
