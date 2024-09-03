ffca98fa959ac54ac6fcbaf586484edc3450f7c2
kale <18kmkf@queensu.ca>
2024-09-03 15:42:07 -0400

    Enhance projects page & add more descriptions

--- app/Projects.js
+++ app/Projects.js
@@ -1,43 +1,70 @@
 import React from "react";
 import ProjectTile from "@/components/ProjectTile";
-import QWIC from "../public/QWICwebsite.png";
+import QWIC from "..//public/QWICwebsite.png";
 const Projects = () => {
   return (
-    <div className="h-fit px-44 pb-12 pt-12">
+    <div className="h-fit px-44 pb-12 pt-12" id="projects">
       <h1 className="font-semibold text-[40px] text-center">
-        Projects I've Done
+        Featured Projects
       </h1>
       <div className="flex flex-col gap-12 pt-12">
         <ProjectTile
           subtitle="project for QWIC"
-          title="Club Website"
-          description="Designed and programmed QWIC's webpage to communicate our mission statement to potential sponsors and to students."
-          thumbnail={QWIC}
+          title="QWIC Club Website"
+          description="This project involved designing and developing a comprehensive website for a university club. The goal was to create a visually appealing, user-friendly platform that aligns with the club's brand identity and meets accessibility standards."
+          contributions={[
+            "Led the design and development process, creating intuitive and visually cohesive web pages that reflect the club's identity",
+            "Leveraged Next.js, Tailwind CSS, and Framer Motion to develop a responsive, dynamic frontend, providing an engaging user experience across all devices",
+            "Utilized Prisma and PostgreSQL to build a robust backend, ensuring efficient data management and seamless functionality.",
+          ]}
+          // thumbnail={}
         />
         <ProjectTile
           subtitle="project for COMPSA"
-          title="Admin Panel"
-          description="Designed and developed a responsive Admin panel for the COMPSA website to allow authorized users to interact with the API and display the database in an intuitive and user-friendly way."
-          thumbnail={QWIC}
+          title="COMPSA Admin Panel"
+          description="This project involved developing a responsive Admin Panel for the COMPSA website, enabling real-time updates to the database. The panel was designed to streamline content management and enhance user experience for administrators."
+          contributions={[
+            "Designed and developed a responsive Admin Panel using Next.js, Bulma CSS, and Docker, ensuring seamless access and updates across various device",
+            "Implemented secure user authentication to safeguard the admin functionalities and data integrity",
+          ]}
+          // thumbnail={QWIC}
         />
         <ProjectTile
           subtitle="Game Design Project (CISC226)"
-          title="The Pentagram"
-          description="The Pentagram is a detective game set on an old desktop computer. Navigate the computer to find clues and solve mysteries to discover what the secret organization, 'The Pentagram' has been up to and why they've been doing it."
-          thumbnail={QWIC}
+          title="Video Game Design Project - The Pentagram"
+          description="In my second year of computing, I took the Video Game Design course (CISC226), which required a year-long group project to create a video game from concept to completion. Our team developed 'The Pentagram', a detective-themed game that incorporated various gameplay mechanics and narrative elements."
+          contributions={[
+            "Collaboratively developed the game's art assets, working closely with team members to create visually engaging graphics and animations that brought the game's world to life.",
+            "Played a key role in crafting the game's storyline and puzzles, ensuring a cohesive and immersive experience for players by working alongside peers to balance challenge and entertainment.",
+          ]}
+          // thumbnail={QWIC}
         />
-        <ProjectTile subtitle="QHacks Project" title=" __ App" description="" />
+        {/* <ProjectTile
+          subtitle="QHacks App Project"
+          title=" QHACKS Project - Productivity App"
+          description=""
+        /> */}
         <ProjectTile
           subtitle="Team project for QWEB"
-          title="Club Website"
+          title="QVSA Club Website"
           description="Designed and programmed QVSA's webpage to communicate their club to students."
-          thumbnail={QWIC}
+          contributions={[
+            "Led the design and development process, creating intuitive and visually cohesive web pages that reflect the club's identity",
+            "Leveraged Next.js, Tailwind CSS, and Framer Motion to develop a responsive, dynamic frontend, providing an engaging user experience across all devices",
+            "Utilized Prisma and PostgreSQL to build a robust backend, ensuring efficient data management and seamless functionality.",
+          ]}
+          // thumbnail={QWIC}
         />
         <ProjectTile
           subtitle="HCI Design Project (CISC)"
-          title="Prescence"
+          title="UI/UX Design Project - Prescence"
           description="Prototype of an application aiming to encourage students to attend class - and keep going!"
-          thumbnail={QWIC}
+          contributions={[
+            "Led the design and development process, creating intuitive and visually cohesive web pages that reflect the club's identity",
+            "Leveraged Next.js, Tailwind CSS, and Framer Motion to develop a responsive, dynamic frontend, providing an engaging user experience across all devices",
+            "Utilized Prisma and PostgreSQL to build a robust backend, ensuring efficient data management and seamless functionality.",
+          ]}
+          // thumbnail={QWIC}
         />
       </div>
     </div>
