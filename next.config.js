24b5e84a08efa47c9d73bcc87de01434dfcf5155
kale <18kmkf@queensu.ca>
2024-09-03 17:12:35 -0400

    config updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: { unoptimized: true }
-  }
-   
-  module.exports = nextConfig
+  output: "export",
+  basePath: "/kale-v2",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
