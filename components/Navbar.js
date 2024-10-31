b8d934a27a82ae1b5ff35bbbaba473dc77f93821
kale <18kmkf@queensu.ca>
2024-10-31 14:16:59 -0400

    feat: :sparkles: center & make navbar smaller

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,35 +10,37 @@ const Navbar = () => {
   console.log(isHomePage);
 
   return (
-    <div className="flex flex-row mx-44 justify-between text-secondary-col pt-6 font-medium">
-      {isHomePage ? (
-        <>
-          <a className="hover:text-[#C6D794]" href="#experience">
-            Experience
-          </a>
-          <a className="hover:text-[#C6D794]" href="#projects">
-            Projects
-          </a>
-        </>
-      ) : (
-        <>
-          <Link className="hover:text-[#C6D794]" href="/">
-            Experience
-          </Link>
-          <Link className="hover:text-[#C6D794]" href="/">
-            Projects
-          </Link>
-        </>
-      )}
-      <Link className="hover:text-[#C6D794]" href="/aboutme">
-        About Me
-      </Link>
-      <a
-        href="mailto:kaleighfeder@gmail.com"
-        className="text-secondary-col hover:text-[#C6D794]"
-      >
-        Contact Me
-      </a>
+    <div className="w-1/3">
+      <div className="flex flex-row justify-between pt-6 font-medium font-poppins">
+        {isHomePage ? (
+          <>
+            <a className="hover:text-[#C6D794]" href="#experience">
+              Experience
+            </a>
+            <a className="hover:text-[#C6D794]" href="#projects">
+              Projects
+            </a>
+          </>
+        ) : (
+          <>
+            <Link className="hover:text-[#C6D794]" href="/">
+              Experience
+            </Link>
+            <Link className="hover:text-[#C6D794]" href="/">
+              Projects
+            </Link>
+          </>
+        )}
+        <Link className="hover:text-[#C6D794]" href="/aboutme">
+          About Me
+        </Link>
+        <a
+          href="mailto:kaleighfeder@gmail.com"
+          className=" hover:text-[#C6D794]"
+        >
+          Contact Me
+        </a>
+      </div>
     </div>
   );
 };
