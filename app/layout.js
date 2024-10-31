025f305191554b317864b71e1e58c6d7280434ee
kale <18kmkf@queensu.ca>
2024-10-31 14:15:50 -0400

    feat: :sparkles: Add new fonts

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,38 @@
 import "./globals.css";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Poppins } from "next/font/google";
+import { Cormorant_Garamond } from "next/font/google";
+import { Imperial_Script } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["400", "500", "600"],
+  variable: "--font-poppins",
+  subsets: ["latin"],
+});
+
+const CormorantGaramond = Cormorant_Garamond({
+  weight: "400",
+  variable: "--font-cormorant-garamond",
+  subsets: ["latin"],
+});
+const ImperialScript = Imperial_Script({
+  weight: "400",
+  variable: "--font-imperial-script",
+  subsets: ["latin"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={plusJakartaSans.className}>
-        <Navbar />
+      <body
+        className={`${CormorantGaramond.variable} ${ImperialScript.variable} ${poppins.variable}`}
+      >
+        <div className="flex justify-center w-full">
+          <Navbar />
+        </div>
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
